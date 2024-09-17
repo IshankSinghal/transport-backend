f32a8efb3683ab1965389727406d06fb346a1137
Anubhav Chaurasia <anubhavchaurasia.ac5@gmail.com>
2024-09-17 10:17:30 +0530

    added roles

--- services/authentication.js
+++ services/authentication.js
@@ -4,6 +4,7 @@ function createTokenForUser(user) {
   const payload = {
     _id: user._id,
     email: user.email,
+    role: user.role,
   };
   const token = JWT.sign(payload, process.env.JWT_SECRET);
   return token;
