8ed06e3a2197de9e7a7f3485a8f29322c1c5c3cb
Ishank Singhal <singhalishank004@gmail.com>
2024-10-14 12:12:17 +0530

    shipment routes

--- routes/shipmentRoutes.js
+++ routes/shipmentRoutes.js
@@ -2,10 +2,28 @@
 
 const express = require("express");
 const router = express.Router();
-const { createShipment } = require("../controller/shipmentController"); // Adjust the path as needed
+const {
+  createShipment,
+  getAllShipments,
+  getShipmentById,
+  updateShipment,
+  deleteShipment,
+} = require("../controller/shipmentController"); // Adjust the path as needed
 const { authMiddleware } = require("../middleware/authMiddleware");
 
 // Create a new shipment
 router.post("/create", createShipment);
+
+//Get All Shipments
+router.get("/", getAllShipments);
+
+//Get A Shipment by Id
+router.get("/:shipmentId", getShipmentById);
+
+//To Updata A Shipment
+router.put("/:shipmentId", updateShipment);
+
+//To Delete A SHipment
+router.delete("/:shipmentId", deleteShipment);
 
 module.exports = router;
